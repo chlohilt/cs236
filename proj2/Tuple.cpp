7291f2fb9e22d4c6931805ea2ad3962671a68a4a
chlohilt <hilton.chloe.y@gmail.com>
2023-03-13 23:52:11 -0600

    After blood, sweat, and tears I'm done

--- proj2/Tuple.cpp
+++ proj2/Tuple.cpp
@@ -16,16 +16,16 @@ string Tuple::toString(const Scheme& scheme) const {
     vector<string> schemeNonConstants;
     int numberOfGoodIterations = 0;
     // counting how many are constants
-    for (int nameValue = 0; nameValue < scheme.names.size(); nameValue++) {
-        if ((scheme.names.at(nameValue)).find("'") == -1) {
+    for (int nameValue = 0; unsigned(nameValue) < scheme.names.size(); nameValue++) {
+        if (int((scheme.names.at(nameValue)).find("'")) == -1) {
             schemeNonConstants.push_back(scheme.names.at(nameValue));
         }
     }
     int sizePrint = 0;
     vector<string> visitedAlreadyPartTwo;
     // checking how big the list is I need to print
-    for (int nameValue = 0; nameValue < scheme.names.size(); nameValue++) {
-        if ((scheme.names.at(nameValue)).find("'") == -1) {
+    for (int nameValue = 0; unsigned(nameValue) < scheme.names.size(); nameValue++) {
+        if (int((scheme.names.at(nameValue)).find("'")) == -1) {
             vector<string>::iterator it = find(visitedAlreadyPartTwo.begin(), visitedAlreadyPartTwo.end(), scheme.names.at(nameValue));
             if (it == visitedAlreadyPartTwo.end()) {
                 sizePrint++;
@@ -34,8 +34,8 @@ string Tuple::toString(const Scheme& scheme) const {
         visitedAlreadyPartTwo.push_back(scheme.names.at(nameValue));
     }
 
-    for (int nameValue = 0; nameValue < scheme.names.size(); nameValue++) {
-        if ((scheme.names.at(nameValue)).find("'") == -1) {
+    for (int nameValue = 0; unsigned(nameValue) < scheme.names.size(); nameValue++) {
+        if (int((scheme.names.at(nameValue)).find("'")) == -1) {
             if (numberOfGoodIterations == 0) {
                 out << "  ";
             }
--- proj2/main.cpp
+++ proj2/main.cpp
@@ -46,63 +46,14 @@ int main(int argc, char** argv) {
     if (argc != 2) {
         return 1;
     }
-    string inputString = "\n"
-                         "\n"
-                         "# repeated two-var selects\n"
-                         "\n"
-                         "\n"
-                         "Schemes:\n"
-                         "\n"
-                         "  a(x,y,z,w)\n"
-                         "  b(x,y,z,w,v)\n"
-                         "\n"
-                         "Facts:\n"
-                         "\n"
-                         "  a('1','2','3','4').\n"
-                         "  a('3','2','4','1').\n"
-                         "  a('2','2','4','4').\n"
-                         "  a('2','2','4','4').\n"
-                         "  a('4','2','3','2').\n"
-                         "  a('4','4','4','4').\n"
-                         "  a('2','2','2','2').\n"
-                         "  a('4','4','2','2').\n"
-                         "\n"
-                         "  b('1','2','3','4','5').\n"
-                         "  b('5','2','2','4','2').\n"
-                         "  b('3','4','3','4','3').\n"
-                         "  b('2','1','2','1','2').\n"
-                         "  b('2','1','2','1','2').\n"
-                         "  b('2','2','4','4','4').\n"
-                         "  b('2','2','4','2','2').\n"
-                         "\n"
-                         "Rules:\n"
-                         "\n"
-                         "Queries:\n"
-                         "\n"
-                         "  a(x,x,x,x)?\n"
-                         "  a(x,x,y,y)?\n"
-                         "  a(x,y,x,y)?\n"
-                         "  a(A,B,B,A)?\n"
-                         "  a(x,x,'1',x)?\n"
-                         "  a(x,'2',x,x)?\n"
-                         "\n"
-                         "  b(x,x,x,x,x)?\n"
-                         "  b(x,y,x,y,x)?\n"
-                         "  b(x,x,x,z,z)?\n"
-                         "  b(z,z,x,z,z)?\n"
-                         "  b(x,x,z,z,z)?\n"
-                         "  b(x,z,z,z,z)?\n"
-                         "  b(x,x,'4',x,x)?\n"
-                         "  b(x,'2',x,'2',x)?\n"
-                         "\n"
-                         "";
-    /*string inputFileName = argv[1];
+    string inputString = "";
+    string inputFileName = argv[1];
     ifstream input(inputFileName);
 
     while (input.peek() != EOF) {
         inputString += input.get();
     }
-    input.close();*/
+    input.close();
 
     Scanner s = Scanner(inputString);
     queue<Token> tokens = s.mainScanner();
